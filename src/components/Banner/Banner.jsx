9ee0a06852781ea02ac44c882ddaebb8f9543b84
ranamiah221 <ranarasul21@gmail.com>
2024-10-19 12:29:34 +0600

    added banner in home page

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,14 +1,30 @@
+import Swal from 'sweetalert2';
 import myImage from '../../assets/banner/mobile_phone.jpg'
+import { useState } from 'react';
 
 const Banner = () => {
+  const [inputValue, setInputValue]=useState('')
+  const handleSearch=()=>{
+    Swal.fire({
+      title: "Sorry!",
+      text: "This Functionality is not available",
+      icon: "error"
+    });
+    setInputValue('')
+  }
 
     return (
         <div
         className="bg-cover bg-center h-96 opacity-75 mt-1 rounded-lg shadow-lg shadow-sky-500 flex justify-center items-center"
         style={{ backgroundImage: `url(${myImage})` }}
       >
-        <div className='w-5/6 mx-auto'>
-            <input className='bg-white w-full rounded-md h-10' type="text" />
+        <div className='flex items-center justify-end w-5/6 mx-auto h-20 static'>
+        <input onChange={(e)=>{
+          setInputValue(e.target.value)
+        }} className='bg-white w-full rounded-md h-16 pl-5' type="text" placeholder='Search..... ' value={inputValue} />
+            <div className='absolute mr-4'>
+            <button onClick={handleSearch} className='btn btn-primary'>Search</button>
+            </div>
         </div>
       </div>
     );
