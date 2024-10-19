80b2d53c7f1665ad8929919a16eea01ed7f8da97
ranamiah221 <ranarasul21@gmail.com>
2024-10-19 13:13:21 +0600

    dynamic page title added for every page

--- src/components/PhoneCard/PhoneCard.jsx
+++ src/components/PhoneCard/PhoneCard.jsx
@@ -27,7 +27,7 @@ const PhoneCard = ({ phone }) => {
           <p className="text-cyan-600 text-base font-semibold">${price}</p>
         </div>
 
-        <Link to={`/phones/${id}`}>
+        <Link to={`/phones/${id}`} state={`${model}`}>
         <button
           className="rounded-md w-full mt-2 bg-cyan-600 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-cyan-700 focus:shadow-none active:bg-cyan-700 hover:bg-cyan-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
           type="button"
--- src/layouts/MainLayouts/MainLayouts.jsx
+++ src/layouts/MainLayouts/MainLayouts.jsx
@@ -1,9 +1,23 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "../../components/Navbar/Navbar";
 import Footer from "../../components/Footer/Footer";
+import { useEffect } from "react";
 
 
 const MainLayouts = () => {
+    const location=useLocation();
+    console.log(location, location.pathname);
+    if(location.pathname =='/'){
+        document.title="Phone - Home"
+    }
+    else{
+       document.title=`Phone ${location.pathname.replace('/','- ')}`
+    }
+    if(location.state){
+      document.title= ` ${location.state}`
+    }
+
+    
     return (
         <div className="max-w-7xl mx-auto">
             <Navbar></Navbar>
